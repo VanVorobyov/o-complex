651ce15dfa8362fdbb2bf5fae60ccd422261714d
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-04-16 08:04:04 +0300

    style(cart): add base styles for cart

--- src/entities/Cart/Cart.tsx
+++ src/entities/Cart/Cart.tsx
@@ -1,26 +1,30 @@
 import { FC } from 'react';
 import styles from './Cart.module.scss';
 import { ICartProps } from '@/entities/Cart/types';
+import { Button } from '@/shared/ui/Button/Button';
+import { Input } from '@/shared/ui/Input/Input';
+
 
 export const Cart: FC<ICartProps> = ({ items, total }) => {
   return (
     <div className={styles.cart}>
-      <h2>Корзина</h2>
+      <h2 className={styles.title}>Добавленные товары</h2>
       <ul className={styles.cartItems}>
         {items.map((item) => (
           <li key={item.id} className={styles.cartItem}>
-            <span className={styles.itemName}>{item.title}</span>
-            <span className={styles.itemPrice}>Цена за 1 ед.: ${item.price}</span>
-            <span className={styles.itemQuantity}>Цена за {item.quantity}ед. : {item.totalPrice}</span>
-            <span className={styles.itemQuantity}>Количество: {item.quantity}</span>
+            <span className={styles.itemText}>{item.title}</span>
+            <span className={styles.itemText}>{item.quantity} ед.</span>
+            <span className={styles.itemText}>{item.totalPrice}₽</span>
           </li>
         ))}
       </ul>
-      <div className={styles.cartTotal}>
-        <span className={styles.totalLabel}>Общая стоимость:</span>
-        <span className={styles.totalAmount}>${total}</span>
+      <span className={styles.total}>{total}</span>
+      <div className={styles.button}>
+        <Input name={'phone'} />
+        <Button>
+          <span>Заказать</span>
+        </Button>
       </div>
-      <button className={styles.checkoutButton}>Оформить заказ</button>
     </div>
   );
 };
